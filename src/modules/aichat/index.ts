ed87d227259e14e07aa0100bebf9045d3541cf4a
mai <74494945+chan-mai@users.noreply.github.com>
2024-12-14 05:56:34 +0900

    fix: aichatの文字列がプロンプトに入らないように

--- src/modules/aichat/index.ts
+++ src/modules/aichat/index.ts
@@ -168,7 +168,9 @@ export default class extends Module {
 			this.log('AiChat requested');
 		}
 
-		const question = msg.extractedText;
+const question = msg.extractedText
+							.replace(RegExp(this.name, "i");, '')
+							.trim();
 
 		let text:string, aiChat:AiChat;
 		let prompt:string = '';
